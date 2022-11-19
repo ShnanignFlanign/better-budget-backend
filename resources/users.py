b5e87bd033015d08098f0c2c81e400dcee2d657c
ShnanignFlanign <seanpflan@live.com>
2022-11-19 07:38:52 -0800

    import CORS and cross_origin

--- resources/users.py
+++ resources/users.py
@@ -3,7 +3,7 @@ import models
 from flask import request, jsonify, Blueprint, session
 from flask_bcrypt import generate_password_hash, check_password_hash
 from flask_login import login_user, current_user, logout_user, login_required
-from flask_cors import cross_origin
+from flask_cors import CORS, cross_origin
 from playhouse.shortcuts import model_to_dict
 from datetime import timedelta
 
@@ -67,7 +67,6 @@ def login():
 @login_required
 @cross_origin()
 def logout():
-
     logout_user()
     return jsonify(
         data={},
